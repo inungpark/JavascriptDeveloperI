4afb5749e89a1f5f294e333f67115ff30977ee30
Inu Park <inungpark@gmail.com>
2024-02-28 16:10:01 -0800

    022824

---
+++ 3.3_classes/classes.js
@@ -0,0 +1,19 @@
+// The basic syntax of a class declaration is as follows:
+class ClassName {
+  // The convention is to start the name of the class with a capital letter.
+  constructor() {}
+  // Adding the constructor is optional.
+  method1() {}
+  // There should be no comma between class methods.
+  method2() {}
+  method3() {}
+}
+// The basic syntax of a class expression is as follows:
+// A class expression can be anonymous or named.
+// The name is only visible in the scope of the class expression.
+let ClassName = class OptionalName {
+  constructor() {}
+  method1() {}
+  method2() {}
+  method3() {}
+};
