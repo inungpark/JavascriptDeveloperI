faf91c04886b7435aadbcf4836d485b905b2ab77
Inu Park <inungpark@gmail.com>
2024-03-01 10:10:02 -0800

    030124

--- 3.3_classes/classes.js
+++ 3.3_classes/classes.js
@@ -1,5 +1,5 @@
 // The basic syntax of a class declaration is as follows:
-class ClassName {
+class ClassNameDeclaration {
   // The convention is to start the name of the class with a capital letter.
   constructor() {}
   // Adding the constructor is optional.
@@ -8,12 +8,51 @@ class ClassName {
   method2() {}
   method3() {}
 }
+
 // The basic syntax of a class expression is as follows:
 // A class expression can be anonymous or named.
 // The name is only visible in the scope of the class expression.
-let ClassName = class OptionalName {
+let ClassNameExpression = class OptionalName {
   constructor() {}
   method1() {}
   method2() {}
   method3() {}
 };
+
+// In this example, the 'MeetingRoom' class is used to create different meeting room objects.
+class MeetingRoom {
+  constructor(available) {
+    if (available) {
+      this.reservable = true;
+    }
+  }
+}
+// An instance of 'MeetingRoom' is created using the 'new' keyword and passing in a parameter.
+let boardRoom = new MeetingRoom(true);
+console.log(boardRoom);
+
+// In this example, the class 'User' is set up to create user objects.
+class User {
+  constructor(user, email) {
+    this.user = user;
+    // User and email are assigned to the instance using 'this'.
+    this.email = email;
+  }
+}
+// An instance of a 'User' is created by passing 'user' and 'email' as parameters.
+let user1 = new User("team_lead", "team@cosmicsoft.com");
+console.log(user1);
+
+// In this example, an instance of the 'User' class is created.
+// Then the 'fullName' method is invoked on the object.
+class User2 {
+  constructor(fName, lName) {
+    this.firstName = fName;
+    this.lastName = lName;
+  }
+  fullName() {
+    return this.firstName + "" + this.lastName;
+  }
+}
+let user2 = new User2("Sarah", "James");
+console.log(user2.fullName());
